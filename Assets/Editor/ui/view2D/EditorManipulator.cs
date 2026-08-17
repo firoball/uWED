1895c43b5e7da8b8581348f2c2e26dc3e999cf04
firoball <dev@firoball.de>
2026-08-17 22:30:22 +0200

    introduce view2D namespce

---
+++ Assets/Editor/ui/view2D/EditorManipulator.cs
@@ -0,0 +1,278 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Editor.UI.View2D
+{
+    public class EditorManipulator : MouseManipulator
+    {
+        private MapData m_mapData; //TODO: move to EditorView?
+        private EditorStatus.Construct m_constructMode;
+        private EditorStatus.Mode m_mode;
+        private List<BaseEditorMode> m_editorModes;
+
+        private const string c_defaultAsset = "assets/DefaultMapAsset.asset";
+
+        public EditorManipulator()
+        {
+            m_mapData = new MapData();
+            m_mode = EditorStatus.Mode.Segments;
+            m_constructMode = EditorStatus.Construct.Idle;
+            LoadPrefs();
+
+            //only construct modes after default map data was loaded
+            m_editorModes = new List<BaseEditorMode>
+            {
+                new ObjectMode(m_mapData),
+                new SegmentMode(m_mapData),
+                new RegionMode(m_mapData),
+                new WayMode(m_mapData)
+            };
+
+            InitializeActiveMode();
+        }
+
+        protected override void RegisterCallbacksOnTarget()
+        {
+            //pass defaults to all listeners
+            EditorView ev = target as EditorView;
+            m_editorModes[(int)m_mode].Drawer.SetEnabled(true);
+            ev?.Interface.NotifySetModeListeners(m_mode);
+
+            //add all editor mode drawers as children
+            foreach (BaseEditorMode em in m_editorModes)
+            {
+                if (em.Drawer != null)
+                    target.Add(em.Drawer);
+            }
+
+            target.RegisterCallback<MouseDownEvent>(OnMouseDown);
+            target.RegisterCallback<MouseMoveEvent>(OnMouseMove);
+            target.RegisterCallback<MouseCaptureOutEvent>(OnMouseOut);
+            target.RegisterCallback<MouseUpEvent>(OnMouseUp);
+        }
+
+        protected override void UnregisterCallbacksFromTarget()
+        {
+            target.UnregisterCallback<MouseDownEvent>(OnMouseDown);
+            target.UnregisterCallback<MouseUpEvent>(OnMouseUp);
+            target.UnregisterCallback<MouseMoveEvent>(OnMouseMove);
+            target.UnregisterCallback<MouseCaptureOutEvent>(OnMouseOut);
+        }
+
+        private void OnMouseOut(MouseCaptureOutEvent evt)
+        {
+
+        }
+
+        private void OnMouseMove(MouseMoveEvent evt)
+        {
+            var editorView = target as EditorView;
+            if (editorView != null)
+            {
+                Vector2 snappedPos = editorView.SnapWorldPos(editorView.ScreenToWorldSpace(evt.localMousePosition));
+                editorView.Interface.NotifyMouseMoveListeners(snappedPos);
+            }
+
+            m_editorModes[(int)m_mode].Drawer.SetLocalMousePosition(evt.localMousePosition);
+
+            EditorStatus.Construct lastconstructMode = m_constructMode;
+            if (m_constructMode == EditorStatus.Construct.Idle)
+            {
+                //middle mouse button pressed
+                if ((evt.pressedButtons & 4) != 0) //Content Dragger is active
+                {
+                    m_constructMode = EditorStatus.Construct.Moving;
+                }
+                //if left mouse is pressed 
+                else if ((evt.pressedButtons & 1) != 0)
+                {
+                    if (m_editorModes[(int)m_mode].StartDrag(evt.ctrlKey))
+                    {
+                        m_constructMode = EditorStatus.Construct.Dragging;
+                    }
+                    else
+                    {
+                        if (!evt.ctrlKey) // ctrl key allows multi select
+                            m_editorModes[(int)m_mode].ClearSelection();
+                        m_editorModes[(int)m_mode].StartSelection();
+                        m_constructMode = EditorStatus.Construct.Selecting;
+                    }
+
+                }
+                else
+                {
+                    //do nothing
+                }
+            }
+
+            if (m_constructMode != lastconstructMode)
+            {
+                editorView?.Interface.NotifySetConstructionModeListeners(m_constructMode);
+            }
+        }
+
+        private void OnMouseUp(MouseUpEvent evt)
+        {
+            target.ReleaseMouse();
+
+            var t = target as EditorView;
+            Vector2 mouseWorldPos = t.ScreenToWorldSpace(evt.localMousePosition);
+            Vector2 mouseSnappedWorldPos = t.SnapWorldPos(mouseWorldPos);
+
+            EditorStatus.Construct lastconstructMode = m_constructMode;
+            switch (m_constructMode)
+            {
+                case EditorStatus.Construct.Idle: //start construction
+                {
+                    switch (evt.button)
+                    {
+                        case 0: //left mousebutton
+                            if (!evt.ctrlKey) //ctrl triggers selection, don't start construction
+                            {
+                                if (!m_editorModes[(int)m_mode].ClearSelection()) //clear current selection
+                                {
+                                    if (!m_editorModes[(int)m_mode].StartConstruction(mouseSnappedWorldPos))
+                                        m_constructMode = EditorStatus.Construct.Constructing;
+                                }
+                            }
+                            else
+                            {
+                                //select single element, add to current selection
+                                m_editorModes[(int)m_mode].SingleSelection();
+                            }
+
+                            break;
+
+                        case 1: //right mousebutton
+                            if (!evt.ctrlKey)
+                                m_editorModes[(int)m_mode].EditObject(t);
+                            else
+                                m_editorModes[(int)m_mode].DeleteObject();
+                            break;
+
+                        case 2: //middle mousebutton
+                            if (!evt.ctrlKey)
+                                m_editorModes[(int)m_mode].ModifyObject(mouseWorldPos, t);
+                            else
+                                m_editorModes[(int)m_mode].ModifyObjectAlt(mouseWorldPos, t);
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+                    break;
+
+                case EditorStatus.Construct.Constructing: //in construction
+                    if (evt.button == 0) //left mousebutton
+                    {
+                        if (m_editorModes[(int)m_mode].ProgressConstruction(mouseSnappedWorldPos))
+                            m_constructMode = EditorStatus.Construct.Idle; // construction finished
+                    }
+                    else if (evt.button == 1) //right mousebutton
+                    {
+                        if (m_editorModes[(int)m_mode].RevertConstruction())
+                            m_constructMode = EditorStatus.Construct.Idle; // construction aborted
+                    }
+
+                    break;
+
+                case EditorStatus.Construct.Dragging: // finish drag mode
+                    if (evt.button == 0) //left mousebutton
+                    {
+                        m_editorModes[(int)m_mode].FinishDrag(mouseSnappedWorldPos);
+                        m_constructMode = EditorStatus.Construct.Idle;
+                    }
+                    else if (evt.button == 1) //right mousebutton
+                    {
+                        m_editorModes[(int)m_mode].AbortDrag();
+                        //m_constructMode will be set to idle with release of left mousebutton only
+                    }
+
+                    break;
+
+                case EditorStatus.Construct.Selecting: // finish selection mode
+                    if (evt.button == 0) //left mousebutton
+                    {
+                        m_editorModes[(int)m_mode].FinishSelection();
+                        m_constructMode = EditorStatus.Construct.Idle;
+                    }
+
+                    break;
+
+                case EditorStatus.Construct.Moving: // finish move mode
+                    if (evt.button == 2) //middle mousebutton
+                    {
+                        m_constructMode = EditorStatus.Construct.Idle;
+                    }
+
+                    break;
+
+                default:
+                    break;
+            }
+
+            if (m_constructMode != lastconstructMode)
+            {
+                EditorView ev = target as EditorView;
+                ev?.Interface.NotifySetConstructionModeListeners(m_constructMode);
+            }
+        }
+
+        private void OnMouseDown(MouseDownEvent evt)
+        {
+            target.CaptureMouse();
+        }
+
+        private void InitializeActiveMode()
+        {
+            m_editorModes[(int)m_mode].Initialize();
+            m_constructMode = EditorStatus.Construct.Idle;
+            EditorView ev = target as EditorView;
+            ev?.Interface.NotifySetModeListeners(m_mode);
+            ev?.Interface.NotifySetConstructionModeListeners(m_constructMode);
+        }
+
+        public void SetMode(EditorStatus.Mode mode)
+        {
+            if (mode != m_mode)
+            {
+                m_editorModes[(int)m_mode].Drawer.SetEnabled(false);
+                m_mode = mode;
+                InitializeActiveMode();
+                m_editorModes[(int)m_mode].Drawer.SetEnabled(true);
+            }
+        }
+
+        public void LoadMap(IMapLoader loader, string name)
+        {
+            if (loader != null && !string.IsNullOrWhiteSpace(name))
+            {
+                m_mapData?.Load(loader, name);
+                InitializeActiveMode(); //map data has changed rebuild all dependent structures
+            }
+        }
+
+        public void WriteMap(IMapWriter writer, string name)
+        {
+            if (writer != null && !string.IsNullOrWhiteSpace(name))
+                m_mapData?.Write(writer, name);
+        }
+
+        public void SavePrefs()
+        {
+            m_mapData?.Write(new MapAssetWriter(), c_defaultAsset);
+            EditorPrefs.SetFloat("uWED::EditorManipulator::mode", (int)m_mode);
+        }
+
+        private void LoadPrefs()
+        {
+            m_mapData?.Load(new MapAssetLoader(), c_defaultAsset);
+            if (EditorPrefs.HasKey("uWED::EditorManipulator::mode"))
+                m_mode = (EditorStatus.Mode)EditorPrefs.GetFloat("uWED::EditorManipulator::mode");
+        }
+
+    }
+}
---
+++ Assets/Editor/ui/view2D/EditorView.cs
@@ -0,0 +1,209 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Editor.UI.View2D
+{
+    public class EditorView : GridView
+    {
+        const float c_pixelsPerUnit = 1f;
+        const bool c_invertYPosition = true;
+
+        private readonly GridManipulator m_gridManipulator;
+        private readonly EditorManipulator m_editorManipulator;
+        private readonly EditorInterface m_interface;
+        private float m_lockAngle = 15.0f; //in deg
+        private bool m_enableSnapping;
+
+        public EditorInterface Interface => m_interface;
+
+        public EditorView()
+        {
+            m_enableSnapping = true;
+            GridBackground grid = new GridBackground();
+            m_gridManipulator = new GridManipulator(grid);
+            m_editorManipulator = new EditorManipulator();
+            m_interface = new EditorInterface(this, m_gridManipulator, m_editorManipulator);
+            name = "EditorView";
+            this.StretchToParentSize();
+            this.AddManipulator(m_gridManipulator); //must be added before Zoomer setup
+            SetupZoom(ContentZoomer.DefaultMinScale * 0.1f, ContentZoomer.DefaultMaxScale * 4.0f);
+            m_gridManipulator.RegisterCallbacksLate(); //must be registered after Zoomer setup
+            Add(grid);
+            this.AddManipulator(new ContentDragger());
+            this.AddManipulator(m_editorManipulator);
+
+            //TODO: don't load transform prefs when map is new or has changed
+            LoadPrefs();
+            //pass defaults to all listeners
+            m_interface.NotifyLockAngleListeners(m_lockAngle);
+            m_interface.NotifyToggleSnappingListeners(m_enableSnapping);
+
+            //this.generateVisualContent += GenerateVisualContent; //TODO: use this hook for drawing textured regions
+            contentViewContainer.BringToFront();
+            //TODO: only perform schedule.Execute when prefs were not found/loaded (e.g. new map)
+            schedule.Execute(() =>
+            {
+                contentViewContainer.style.translate = parent.worldBound.size / 2f;
+                //TODO: don't load transform prefs when map is new or has changed
+                LoadPrefs();
+            });
+        }
+
+        /*void GenerateVisualContent(MeshGenerationContext m) //TODO: use this hook for drawing textured regions
+        {
+            Debug.Log("GenerateVisualContent");
+        }*/
+
+        public Matrix4x4 WorldToScreenMatrix()
+        {
+            //layout offset - this is already in screen coordinates
+            Vector3 layoutTranslate =
+                new Vector3(contentViewContainer.layout.position.x, contentViewContainer.layout.position.y);
+            //invert y if configured
+            Vector3 s = new Vector3(1, 1, 1);
+            if (c_invertYPosition)
+                s.y = -s.y;
+            //configured pixel resolution
+            Vector3 pixelScale = new Vector3(c_pixelsPerUnit, c_pixelsPerUnit);
+            //build actual world to screen matrix
+            return GetViewContainerMatrix() * Matrix4x4.Translate(layoutTranslate) * Matrix4x4.Scale(s) *
+                   Matrix4x4.Scale(pixelScale);
+        }
+
+        public Vector2 WorldtoScreenSpace(Vector2 pos)
+        {
+            var position = TransformScreenPos(pos * c_pixelsPerUnit - contentViewContainer.layout.position);
+            return GetViewContainerMatrix().MultiplyPoint3x4(position);
+        }
+
+        public Vector2 TransformScreenPos(Vector2 pos)
+        {
+            if (c_invertYPosition)
+                pos.y = -pos.y;
+            return pos;
+        }
+
+        public Vector2 ScreenToWorldSpace(Vector2 pos)
+        {
+            Vector2 position = TransformScreenPos(GetViewContainerMatrix().inverse.MultiplyPoint3x4(pos));
+            return (position + contentViewContainer.layout.position) / c_pixelsPerUnit;
+        }
+
+        public float ScaleScreenToWorld(float length)
+        {
+            return length / contentViewContainer.resolvedStyle.scale.value.x / c_pixelsPerUnit;
+        }
+
+        public Vector2 SnapWorldPos(Vector2 pos)
+        {
+            if (m_enableSnapping)
+            {
+                Vector2 fac = pos / m_gridManipulator.GridSpacing;
+                int snapX = (fac.x < 0) ? (int)(fac.x - 0.5f) : (int)(fac.x + 0.5f);
+                int snapY = (fac.y < 0) ? (int)(fac.y - 0.5f) : (int)(fac.y + 0.5f);
+                Vector2 intfac = new Vector2(snapX, snapY);
+                return intfac * m_gridManipulator.GridSpacing;
+            }
+            else
+            {
+                return pos;
+            }
+        }
+
+        public Vector2 SnapScreenPos(Vector2 pos)
+        {
+            if (m_enableSnapping)
+            {
+                Vector2 worldPos = ScreenToWorldSpace(pos);
+                Vector2 snappedPos = SnapWorldPos(worldPos);
+                return WorldtoScreenSpace(snappedPos);
+            }
+            else
+            {
+                return pos;
+            }
+        }
+
+        public float SnapAngle(float angle) //angle in rad
+        {
+            if (m_enableSnapping)
+            {
+                float degrees = angle * 180 / Mathf.PI; //angle in deg
+                int snapped;
+                if (degrees > 0.0f)
+                    snapped = (int)((degrees + 0.5f * m_lockAngle) / m_lockAngle);
+                else
+                    snapped = (int)((degrees - 0.5f * m_lockAngle) / m_lockAngle);
+                angle = snapped * m_lockAngle;
+                if (angle <= -180f) angle = 180f; //edge case: -180 deg -> 180 deg
+                return (angle / 180) * Mathf.PI; //angle in rad
+            }
+            else
+            {
+                return angle; //angle in rad
+            }
+        }
+
+        public void ToggleSnapping(bool enable)
+        {
+            m_enableSnapping = enable;
+            m_interface.NotifyToggleSnappingListeners(m_enableSnapping);
+        }
+
+        public void LockAngle(float angle)
+        {
+            m_lockAngle = angle;
+            m_interface.NotifyLockAngleListeners(m_lockAngle);
+        }
+
+        public void SavePrefs()
+        {
+            m_gridManipulator?.SavePrefs();
+            m_editorManipulator?.SavePrefs();
+            EditorPrefs.SetBool("uWED::EditorView::enableSnapping", m_enableSnapping);
+            EditorPrefs.SetFloat("uWED::EditorView::lockAngle", m_lockAngle);
+            EditorPrefs.SetFloat("uWED::EditorView::transform.position.x",
+                contentViewContainer.resolvedStyle.translate.x);
+            EditorPrefs.SetFloat("uWED::EditorView::transform.position.y",
+                contentViewContainer.resolvedStyle.translate.y);
+            EditorPrefs.SetFloat("uWED::EditorView::transform.scale.x",
+                contentViewContainer.resolvedStyle.scale.value.x);
+            EditorPrefs.SetFloat("uWED::EditorView::transform.scale.y",
+                contentViewContainer.resolvedStyle.scale.value.y);
+        }
+
+        private void LoadPrefs()
+        {
+            if (EditorPrefs.HasKey("uWED::EditorView::enableSnapping"))
+                m_enableSnapping = EditorPrefs.GetBool("uWED::EditorView::enableSnapping");
+            if (EditorPrefs.HasKey("uWED::EditorView::lockAngle"))
+                m_lockAngle = EditorPrefs.GetFloat("uWED::EditorView::lockAngle");
+
+            //TODO: load pos and scale only if map has not changed
+            Vector3 pos = contentViewContainer.resolvedStyle.translate;
+            if (EditorPrefs.HasKey("uWED::EditorView::transform.position.x"))
+                pos.x = EditorPrefs.GetFloat("uWED::EditorView::transform.position.x");
+            if (EditorPrefs.HasKey("uWED::EditorView::transform.position.y"))
+                pos.y = EditorPrefs.GetFloat("uWED::EditorView::transform.position.y");
+            contentViewContainer.style.translate = pos;
+
+            Vector3 scale = contentViewContainer.resolvedStyle.scale.value;
+            if (EditorPrefs.HasKey("uWED::EditorView::transform.scale.x"))
+                scale.x = EditorPrefs.GetFloat("uWED::EditorView::transform.scale.x");
+            if (EditorPrefs.HasKey("uWED::EditorView::transform.scale.y"))
+                scale.y = EditorPrefs.GetFloat("uWED::EditorView::transform.scale.y");
+            contentViewContainer.style.scale = scale;
+        }
+
+        private Matrix4x4 GetViewContainerMatrix()
+        {
+            Vector3 t = contentViewContainer.resolvedStyle.translate;
+            Quaternion r = Quaternion.Euler(contentViewContainer.resolvedStyle.rotate.angle.ToDegrees(), 0, 0);
+            Vector3 s = contentViewContainer.resolvedStyle.scale.value;
+
+            //build actual world to screen matrix
+            return Matrix4x4.TRS(t, r, s);
+        }
+    }
+}
